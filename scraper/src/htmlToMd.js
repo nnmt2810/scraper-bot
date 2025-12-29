f71f7239311b849d75b0bd2d0549d6202c75623f
23120102 <nnminhtuan2810@gmail.com>
2025-12-29 20:50:24 +0700

    feat: init htmlToMd using Turndown

---
+++ scraper/src/htmlToMd.js
@@ -0,0 +1,18 @@
+const TurndownService = require('turndown');
+
+const turndownService = new TurndownService({
+  headingStyle: 'atx',
+  codeBlockStyle: 'fenced'
+});
+
+function convert(article) {
+  // Add front matter
+  const header = `---\ntitle: ${article.title}\nurl: ${article.html_url}\n---\n\n`;
+  
+  // Turndown the HTML body to Markdown
+  const markdownBody = turndownService.turndown(article.body);
+  
+  return header + markdownBody;
+}
+
+module.exports = convert;
